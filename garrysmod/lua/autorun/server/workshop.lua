969e58ca732f364d82e0a35c8c55cdbd2935e0d4
snvlplie <snowyy2lie@gmail.com>
2024-01-23 23:10:05 +0200

    workshop.lua instead of resource.lua

---
+++ garrysmod/lua/autorun/server/workshop.lua
@@ -0,0 +1,17 @@
+resource.AddWorkshop ( "2136421687" ) --Brick's Server Framework
+resource.AddWorkshop ( "2126843730" ) --Brick's Essentials - Content
+resource.AddWorkshop ( "2790694511" ) --AHUD
+resource.AddWorkshop ( "2228228831" ) --pCasino - Content 
+resource.AddWorkshop ( "2391537985" ) --[CONTENT] Realistic Police - The Best Realistic Police Script
+resource.AddWorkshop ( "2064194474" ) --gProtect - Content
+resource.AddWorkshop ( "2206993805" ) --slib - Stromic's Library
+resource.AddWorkshop ( "2005357731" ) --[Content] SlownLS - ATM
+resource.AddWorkshop ( "2001211879" ) --realistic house
+resource.AddWorkshop ( "2567098902" ) --accessory
+resource.AddWorkshop ( "1455944872" ) --Character & Clothes
+resource.AddWorkshop ( "2926057708" ) --cmenu content
+resource.AddWorkshop ( "2986127962" ) --map content
+resource.AddWorkshop ( "2986120857" ) --map 
+resource.AddWorkshop( "708845241" ) -- Revival Player Defibrillator | DarkRP Friendly
+resource.AddWorkshop( "3023155302" ) -- The Advanced Hand Swep
+resource.AddWorkshop( "2567098902" ) -- Advanced Accessory System content
