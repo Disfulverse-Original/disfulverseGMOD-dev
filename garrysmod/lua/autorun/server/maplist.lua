d76848e3fafebc638ce320ff58ffad22bdc9630e
snvlplie <snowyy2lie@gmail.com>
2024-02-07 00:01:12 +0200

    Update maplist.lua

--- garrysmod/lua/autorun/server/maplist.lua
+++ garrysmod/lua/autorun/server/maplist.lua
@@ -1,7 +1,7 @@
 local mapIDs = mapIDs or {}
 local curMap = game.GetMap()
  
-mapIDs["rp_downtown_modern"] = 2986120857
+mapIDs["rp_downtown_triton_disfulverse_v2"] = 3155462760
  
 if not mapIDs[curMap] == nil then
  resource.AddWorkshop(mapIDs[map])
