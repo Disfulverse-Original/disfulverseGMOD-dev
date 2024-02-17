a7b204155a9b392b56e5c01b09d22a23dcfd247e
Renz0l1 <60831671+Renz0l1@users.noreply.github.com>
2024-02-17 15:46:01 +0300

    jailroom fix

--- garrysmod/addons/[admin]ulx/lua/ulx/modules/sh/jailroom.lua
+++ garrysmod/addons/[admin]ulx/lua/ulx/modules/sh/jailroom.lua
@@ -1,82 +1,86 @@
-local CATEGORY_NAME = "DarkRP"
+local CATEGORY_NAME = "JailRoom"
 
 if SERVER then
-util.AddNetworkString("ulxTakeJailInfo")
-util.AddNetworkString("ulxTakeUnJailInfo")
+    util.AddNetworkString("ulxTakeJailInfo")
+    util.AddNetworkString("ulxTakeUnJailInfo")
 end
 
---Менять после этой строчки:
-jailPos = Vector(-53.016605, 143.576752, 12720.031250) --позиция комнаты наказаний (чтобы получить, напишите в консколь игры getpos).										  
-jailTeam = TEAM_CITIZEN --работа, на которую будет меняться текущая работа игрока, при заходе в комнату наказаний.
-						--если не хотите менять профессию, просто оставьте ""
--- Дальше не менять!
+-- Менять после этой строчки:
 
-function ulx.jailroom(ply, target, seconds, reason, unjail)
-	if #target <= 2 then
-		for i=1, #target do
-			if unjail == false then
-				local v = target[i]
-				timer.Simple(1,function()
-					JailRoom(v, reason, seconds)
-					if not reason == " " then
-							local str = "#A телепортировал #T в комнату наказаний на #i секунд."
-						ulx.fancyLogAdmin(ply, str, target, seconds)
-					else
-						local str = "#A телепортировал #T в комнату наказаний на #i секунд. Причина: #s."
-						ulx.fancyLogAdmin(ply, str, target, seconds, reason)
-					end
-				end)
-				else
-				local v = target[i]
-				local str = "#A вытащил #T из тюрьмы"
-				ulx.fancyLogAdmin(ply, str, target)
-				UnJail(v)
-			end
-		end
-		else
-			for _,plyr in pairs (player.GetAll()) do
-				ply:ChatPrint("Вы попытались затронуть несколько людей, будьте осторожны!")
-			end
-	end
+-- Позиция где человека будет кидать в джайл
+local jailPos = Vector(2482.687988, -11251.164062, 317.871277)
+-- Работу которую он будет менять когда его будут кидать в джайл
+local jailTeam = TEAM_CITIZEN
+
+local function sendJail(ply, targets, seconds, reason, unjail)
+    if #targets <= 2 then
+        for i = 1, #targets do
+            local v = targets[i]
+            if unjail == false then
+                timer.Simple(1, function()
+                    JailRoom(v, reason, seconds)
+                    local str = unjail and "#A разджайлил #T" or "#A заджайлил #T на #i секунд" .. (reason == " " and "." or ". Причина: #s.")
+                    ulx.fancyLogAdmin(ply, str, targets, seconds, reason)
+                end)
+            else
+                ulx.fancyLogAdmin(ply, "#A разджайлил #T", targets)
+                UnJail(v)
+            end
+        end
+    else
+        ply:ChatPrint("Вы попытались затронуть несколько людей, будьте осторожны!")
+    end
 
     tblCountjail = tblCountjail or {}
-    tblCountjail[ply] = tblCountjail[ply] and tblCountjail[ply] or 0
+    tblCountjail[ply] = tblCountjail[ply] or 0
     tblCountjail[ply] = tblCountjail[ply] + 1
 
-    if tblCountjail and tblCountjail[ply] > 6 then
-        ulx.removeuser( nil, ply )
+    if tblCountjail[ply] > 6 then
+        ulx.removeuser(nil, ply)
     end
 
     timer.Simple(70, function()
         tblCountjail[ply] = nil
     end)
-	
 end
-local jailroom = ulx.command(CATEGORY_NAME, "ulx jailroom", ulx.jailroom, "!jailroom")
-jailroom:addParam{ type=ULib.cmds.PlayersArg }
-jailroom:addParam{ type=ULib.cmds.NumArg, min=0, default=0, hint="Секунды", ULib.cmds.round, ULib.cmds.optional }
-jailroom:addParam{ type=ULib.cmds.StringArg, hint="Причина", ULib.cmds.optional, ULib.cmds.takeRestOfLine}
-jailroom:addParam{ type=ULib.cmds.BoolArg, invisible=true }
-jailroom:defaultAccess( ULib.ACCESS_ADMIN )
-jailroom:setOpposite( "ulx unroom", {_, _, _, _, true}, "!unroom" )
+
+-- Команда ulx jail
+local function ulxJailroomCmd(ply, target, seconds, reason, unjail)
+    sendJail(ply, target, seconds, reason, unjail)
+end
+local jailroom = ulx.command(CATEGORY_NAME, "ulx jailroom", ulxJailroomCmd, "!jailroom")
+jailroom:addParam{ type = ULib.cmds.PlayersArg }
+jailroom:addParam{ type = ULib.cmds.NumArg, min = 0, default = 0, hint = "Секунды", ULib.cmds.round, ULib.cmds.optional }
+jailroom:addParam{ type = ULib.cmds.StringArg, hint = "Причина", ULib.cmds.optional, ULib.cmds.takeRestOfLine }
+jailroom:addParam{ type = ULib.cmds.BoolArg, invisible = true }
+jailroom:defaultAccess(ULib.ACCESS_ADMIN)
+jailroom:setOpposite("ulx unjailroom", { _, _, _, _, true }, "!unjailroom")
 
 function JailRoom(ply, reason, seconds, after_relog)
 	
 	if ply.jailed == true then return end
+	ply.__switchTeam = false
 	ply.LastPos = ply:GetPos()
 	ply.jailed = true
 	ply.timer = seconds
 	ply.jail_reason = reason
+
+	ply:SetPos(jailPos)
 	timer.Simple(1,function ()
-		if not jailTeam == "" then
-			if not ply:Team() == jailTeam then
-				ply:changeTeam(jailTeam, true)
-			end
+		if not (ply:Team() == jailTeam) then
+			ply:changeTeam(jailTeam, true)
+			ply:Spawn()
+			ply:SetPos(jailPos)
+			timer.Simple(1,function ()
+				ply.__switchTeam = true
+			end)
 		end
-		ply:GodEnable()
+		ply:SetMoveType(MOVETYPE_WALK)
 	end)
 	
-	ply:SetPos(jailPos)
+	timer.Simple(1,function ()
+		ply:GodEnable()
+	end)
 	
 	ply:StripWeapons()
 	if timer.Exists(ply:UniqueID().."ulxJailTimer") then
@@ -106,18 +110,29 @@ function UnJail(ply, after_relog)
 			ply.jailed = false
 			ply.LastPos = nil
 		end
+
+		hook.Run("ulxPlayerUnjailed", ply)
+	end
+	
 		timer.Remove(ply:UniqueID().."ulxJailTimer")
 		net.Start("ulxTakeUnJailInfo")
 		net.Send(ply)
 		timer.Simple(5,function ()
-			ply:GodDisable()
 			ply:Spawn()
-			ply:ChatPrint("Наказание закончилось! Пожалуйста, соблюдате правила!")
+			ply:GodDisable()
+			ply:ChatPrint("Ваше наказание закончилось!")
 		end)
 		
 	end
-end
 
+	
+hook.Add("OnPlayerChangedTeam", "ulxForceJailAfterJobChange", function (ply, oldTeam, newTeam)
+    if ply.jailed == true then
+        timer.Simple(0.1, function ()
+            ply:SetPos(jailPos)
+        end)
+    end
+end)
 
 hook.Add("PlayerSpawn","ulxSpawnInJailIfDead",function (ply)
 	if ply.jailed == true then
@@ -131,6 +146,68 @@ hook.Add("CanPlayerSuicide","ulxSuicedeCheck",function (ply)
 	if ply.jailed == true then
 		return false
 	end
+end)
+
+hook.Add("canBuyCustomEntity", "ulxBlockEntityPurchaseInJail", function(ply)
+    if ply.jailed == true then
+        return false, "Вы не можете покупать сущности, пока находитесь в тюрьме!"
+    end
+end)
+
+hook.Add("canBuyAmmo", "ulxBlockAmmoPurchaseInJail", function(ply)
+	if ply.jailed == true then
+		return false, "Вы не можете покупать боеприпасы, пока находитесь в тюрьме!"
+	end
+end)
+	
+hook.Add("canBuyShipment", "ulxBlockShipmentPurchaseInJail", function(ply)
+	if ply.jailed == true then
+		return false, "Вы не можете покупать отправки, пока находитесь в тюрьме!"
+	end
+end)
+	
+hook.Add("canBuyPistol", "ulxBlockPistolPurchaseInJail", function(ply)
+	if ply.jailed == true then
+		return false, "Вы не можете покупать пистолеты, пока находитесь в тюрьме!"
+	end
+end)
+	
+hook.Add("canBuyVehicle", "ulxBlockVehiclePurchaseInJail", function(ply)
+	if ply.jailed == true then
+		return false, "Вы не можете покупать транспортные средства, пока находитесь в тюрьме!"
+	end
+end)
+
+hook.Add("EntityTakeDamage", "ulxNoDamageInJail", function(target, dmgInfo)
+	if target:IsPlayer() and target.jailed == true then
+		return true
+	end
+end)
+	
+hook.Add("playerCanChangeTeam", "ulxBlockJobSwitchInJail", function(ply)
+	if ply.__switchTeam == true then
+		if ply.jailed == true then
+			return false, "Вы не можете сменить профессию, пока находитесь в тюрьме!"
+		end
+	end
+end)
+
+hook.Add("PlayerSpawnSWEP", "ulxNoSpawnSWEPsInJail", function(ply)
+    if ply.jailed == true then
+        return false
+    end
+end)
+
+hook.Add("PlayerSpawnVehicle", "ulxNoSpawnVehiclesInJail", function(ply)
+    if ply.jailed == true then
+        return false
+    end
+end)
+
+hook.Add("CanOwnDoor", "ulxNoPurchaseDoorsInJail", function(ply)
+    if ply.jailed == true then
+        return false
+    end
 end)
 
 hook.Add("PlayerSpawnProp","ulxBlockSpawnIfInJail",function (ply)
@@ -151,22 +228,28 @@ hook.Add("PlayerCanPickupItem","ulxPickUpRest",function (ply)
 	end
 end)
 
+hook.Add("OnSpawnMenuOpen", "ulxDisableSpawnMenuInJail", function()
+    if LocalPlayer().jailed == true then
+        return false
+    end
+end)
+
+-- Запретить No-Clip в jail:
+hook.Add("PlayerNoClip", "ulxJailDisableNoclip", function(ply, desiredState)
+    if ply.jailed == true then
+        return false
+    end
+end)
+
 if CLIENT then	
 	surface.CreateFont( "DisplayJailTimer", {
 		font = "Roboto",
 		size = (ScrH() + ScrW()) * .011,
-		weight = 300, 
+		weight = 1000, 
 		blursize = 0, 
 		scanlines = 0, 
-		antialias = false, 
-		underline = false, 
-		italic = false, 
-		strikeout = false, 
-		symbol = false, 
-		rotary = false, 
-		shadow = true, 
-		additive = false, 
-		outline = false,
+		antialias = true,
+		extended = true,
 	} )
 
 	local jailed = false
@@ -181,10 +264,10 @@ if CLIENT then
 		hook.Add("HUDPaint","ulxPaintJailInfo",function()
 	        if jailed and math.Round((jail_curtime + jail_timer) - CurTime()) > 0 then
 				if not jail_reason == " " then
-					local x,y = draw.SimpleText('Вы наказаны! Причина: '.. jail_reason ..'.',"DisplayJailTimer",ScrW()/2,0,Color(255,255,255),1,0)
+					local x,y = draw.SimpleText('Вы заджайлины! Причина: '.. jail_reason ..'.',"DisplayJailTimer",ScrW()/2,0,Color(255,255,255),1,0)
 							draw.SimpleText('Осталось: '..math.Round((jail_curtime + jail_timer) - CurTime()) .. ' секунд.',"DisplayJailTimer",ScrW()/2,y+2,Color(255,255,255),1,0)
 				end
-					local x,y = draw.SimpleText('Вы наказаны! Причина: '.. jail_reason ..' ',"DisplayJailTimer",ScrW()/2,0,Color(255,255,255),1,0)
+					local x,y = draw.SimpleText('Вы заджайлины! Причина: '.. jail_reason ..' ',"DisplayJailTimer",ScrW()/2,0,Color(255,255,255),1,0)
 										draw.SimpleText('Осталось: '..math.Round((jail_curtime + jail_timer) - CurTime()) .. ' секунд.',"DisplayJailTimer",ScrW()/2,y+2,Color(255,255,255),1,0)
 				else
 	        end
@@ -208,7 +291,7 @@ if SERVER then
 		if ply:IsValid() then
 			local query = sql.Query("SELECT * FROM jailed WHERE steamid = "..sql.SQLStr(ply:SteamID()))
 			if query then
-				JailRoom(ply, "Вы переехали, потому что недоступны", query[1]['time'], true)
+				JailRoom(ply, "Вы вышли из сервера.", query[1]['time'], true)
 				sql.Query("DELETE FROM jailed WHERE steamid = '"..ply:SteamID().."'")
 				timer.Simple(5,function ()
 					ply:GodEnable()
@@ -225,3 +308,9 @@ if SERVER then
 		end
 	end)
 end
+
+hook.Add("PlayerSpawnSENT", "ulxNoSpawnPropsInJail", function(ply)
+    if ply.jailed == true then
+        return false
+    end
+end)
