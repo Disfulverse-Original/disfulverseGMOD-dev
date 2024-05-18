c5478b97cb16f536685666c8e4891bf7feae05ff
snvlplie <snowyy2lie@gmail.com>
2024-05-18 18:55:51 +0300

    -----

--- garrysmod/addons/[1gamemode]disfulmod/lua/darkrp_modules/event_dis_command/sh_event.lua
+++ garrysmod/addons/[1gamemode]disfulmod/lua/darkrp_modules/event_dis_command/sh_event.lua
@@ -1,32 +1,37 @@
 
-local col = Color(252, 201, 81)
+if SERVER then
+	local col = Color(252, 207, 109)
+
+	local function Event(ply, args)
+
+		if !ply:IsAdmin() then return end
+
+		if args == "" then
+			DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
+			return ""
+		end
+
+		local DoSay = function(text)
+			if text == "" then
+				DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
+				return ""
+			end
+
+			for _, target in ipairs(player.GetAll()) do
+				DarkRP.talkToPerson(target, col, "[Событие]" .. " : " .. text)
+			end
+		end
+
+		return args, DoSay
+
+	end
+
+	DarkRP.defineChatCommand("event", Event)
+end
+
 
 DarkRP.declareChatCommand{
 	command = "event",
 	description = "Отправка глобального сообщения о каком-либо событии.",
 	delay = 1.5
 }
-
-local function Event(ply, args)
-
-	if args == "" then
-		DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
-		return ""
-	end
-
-	local DoSay = function(text)
-		if text == "" then
-			DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
-			return ""
-		end
-
-		for _, target in ipairs(player.GetAll()) do
-			DarkRP.talkToPerson(target, col, "[Событие]" .. " : " .. text)
-		end
-	end
-
-	return args, DoSay
-
-end
-
-DarkRP.defineChatCommand("event", Event, 1.5)
